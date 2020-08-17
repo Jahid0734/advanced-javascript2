8f1c953030282d1b6d83e6b35743dbc5a18cc176
jahid0734 <jahid0734@gmail.com>
2020-08-17 20:47:19 +0600

    null vs undefind

---
+++ null-vs-undefind.js
@@ -0,0 +1,26 @@
+let bird;
+console.log(bird)
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    // return add
+}
+const result = add(15, 56);
+console.log(result);
+
+
+function add(num1, num2){
+    console.log(num1, num2);
+    // return add
+}
+const result = add(15);
+console.log(result);
+
+const Object = {name:"Jahid Hasan", phone: 01743782856}
+console.log(Object.email);
+
+let name =undefined;
+console.log(name)
+
+const array = [15,20,36,65]
+console.log(array[5]);
