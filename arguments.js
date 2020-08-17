424f8d3092d05353783ebd63a012dd93206fd9bd
jahid0734 <jahid0734@gmail.com>
2020-08-17 23:05:14 +0600

    update

--- arguments.js
+++ arguments.js
@@ -1,12 +1,12 @@
 let total = 0;
 function add(num1, num2, ) {
     const numbers = [...arguments];
-    // for(let i = 0; i < numbers.length; i ++){
-    //     const element = numbers[i];
-    //     total  =  total + element
-    //         console.log(total);
-    // }
+    for(let i = 0; i < numbers.length; i ++){
+        const element = numbers[i];
+        total  =  total + element
+            
+    }
     return total;
 }
-const result = add(2, 3, 5, 7);
-console.log(result)
+const result = add(2, 3, 5, 7,20, 25);
+console.log(total)
