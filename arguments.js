e9dbd1ca1c1217b9f17807f8b4498ae91b915f68
jahid0734 <jahid0734@gmail.com>
2020-08-17 23:01:48 +0600

    arguments added

---
+++ arguments.js
@@ -0,0 +1,12 @@
+let total = 0;
+function add(num1, num2, ) {
+    const numbers = [...arguments];
+    // for(let i = 0; i < numbers.length; i ++){
+    //     const element = numbers[i];
+    //     total  =  total + element
+    //         console.log(total);
+    // }
+    return total;
+}
+const result = add(2, 3, 5, 7);
+console.log(result)
