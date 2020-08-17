997ec64e5196a6b5ebc4c652a9ce0154046a9de1
jahid0734 <jahid0734@gmail.com>
2020-08-17 20:26:00 +0600

    true-false added

---
+++ true-false.js
@@ -0,0 +1,31 @@
+//falsy
+//false
+// 0
+// ""
+// undefined
+// null
+// NaN
+//turthy
+//'0', ' ', []
+
+const name = false;
+
+console.log(name)
+if(name){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
+
+// function evenly(a, b){
+//     if(a % b == 0){ //(a % 2 ==0) / b)
+        
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// console.log(evenly(85, 4))
